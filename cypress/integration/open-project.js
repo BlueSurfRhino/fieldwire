9053782484ddaa15af678e56d26e2b9088a605e0
Richard Owens <richowens@outlook.com>
2022-01-19 20:46:33 -0800

    Completed project creations & deletion

--- cypress/integration/open-project.js
+++ cypress/integration/open-project.js
@@ -1,13 +1,13 @@
 describe("The Home Page", () => {
   it("successful login", () => {
-    cy.visit("https://app.fieldwire.com/"); // change URL to match your dev URL
+    cy.visit("https://app.fieldwire.com/"); 
     cy.get("#email-input").type("richowens@outlook.com");
     cy.get(".btn").click();
     cy.get("#password-input").type("WB22uG*Dt.3c?5Q");
     cy.get(".btn").click();
   });
-  it("new project creation", () => {
-    cy.pause();
+  it("new project creation - no content", () => {
+    //create project
     cy.get(".new-project").click();
     cy.get('[name="name"]').type("Test");
     cy.get('[name="code"]').type("Test");
@@ -16,6 +16,15 @@ describe("The Home Page", () => {
     cy.get(".next-btn").click();
     cy.get(".next-btn").click();
     cy.get(".next-btn").click();
-    //cy.get('[data-test-id="test-example"]')
+    //navigate to home to verify project creation
+    cy.get('.hidden-lg').click();
+    cy.get('.nav-icon').click();
+    cy.get('.truncate > .ng-scope').click();
+    //verify the project we created exists
+    cy.get('[data-e2e="Test-cog"]').invoke('show').click({ force: true });
+    //delete our project
+    cy.get('[ng-click="$ctrl.remove()"]').click();
+    cy.get('form.ng-pristine > .form-control').type("Test");
+    cy.get('[ng-disabled="!enabled()"]').click();
   });
 });
